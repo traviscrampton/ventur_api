80bd91f83a61b105b113282ff1349b6bab99d99e
Travis Crampton <crampton.travis@gmail.com>
2018-12-24 15:43:22 -0800

    spacing

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,9 +31,9 @@ class User < ActiveRecord::Base
   has_many :favorites
 
   def generate_jwt
-    JWT.encode({ id: id,
-                 exp: 365.days.from_now.to_i },
-               Rails.application.secrets.secret_key_base)
+    JWT.encode({  id: id,
+                  exp: 365.days.from_now.to_i },
+                  Rails.application.secrets.secret_key_base)
   end
 
   def avatar_image_url
