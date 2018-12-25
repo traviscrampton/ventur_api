14ab1b3ea485d9b4f11cdc372dbd939035e48d60
Travis Crampton <crampton.travis@gmail.com>
2018-12-24 16:47:13 -0800

    more loggin

--- app/graphql/resolvers/auth/login.rb
+++ app/graphql/resolvers/auth/login.rb
@@ -5,13 +5,16 @@ class Resolvers::Auth::Login < GraphQL::Function
     field :user, Types::UserType
   end
 
-  argument :email, !types.String
-  argument :password, !types.String
+  argument :email, types.String
+  argument :password, types.String
 
 
   def call(obj, args, ctx)
+    Rails.logger.debug("HITTIN IS IT?")
     user = User.find_by_email(args[:email])
+    Rails.logger.debug("HITTIN IS IT? 2")
     if user && user.valid_password?(args[:password])
+      Rails.logger.debug("HITTIN IS IT? 3")
       OpenStruct.new({
         user: user,
         token: user.generate_jwt
