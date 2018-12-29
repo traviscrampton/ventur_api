6c9c06899fddfa9d7b696ad1664e275797b3afda
Travis Crampton <crampton.travis@gmail.com>
2018-12-28 17:13:07 -0800

    journal following on graphql

--- app/controllers/journal_follows_controller.rb
+++ app/controllers/journal_follows_controller.rb
@@ -4,7 +4,7 @@ class JournalFollowsController < ApplicationController
   def create
     if current_user
       if check_for_email_follow_and_update
-        render json: { followed: true }
+        render json: { isFollowing: true }
       end
 
       journal_follow = JournalFollow.new(journal_id: params[:journalId], user_id: current_user.id, user_email: current_user.email)
@@ -13,7 +13,7 @@ class JournalFollowsController < ApplicationController
     end
 
     if journal_follow.save
-      render json: { followed: true }
+      render json: { isFollowing: true }
     else
       render json: { errors: journal_follow.errors.full_messages }, status: 422
     end
@@ -23,7 +23,7 @@ class JournalFollowsController < ApplicationController
     @journal_follow = JournalFollow.find_by(journal_id: params[:journalId], user_email: current_user.email)
 
     if @journal_follow.delete
-      render json: { followed: false }
+      render json: { isFollowing: false }
     else
       render json: { errors: @journal_follow.errors.full_messages }, status: 422
     end
