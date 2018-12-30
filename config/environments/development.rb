81b665ec61729b6f5cc1cfcb017815ac318bea65
Travis Crampton <crampton.travis@gmail.com>
2018-12-29 16:49:13 -0800

    find it

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,7 +60,4 @@ Rails.application.configure do
     authentication:       'plain',
     enable_starttls_auto: true,
   }
-
-
-  config.action_mailer.default_url_options = { host: "ventur.co" }
 end
