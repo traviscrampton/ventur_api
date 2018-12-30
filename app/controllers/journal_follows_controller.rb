33d8c8c5ffbebc5210f1d966db801d35eca55d39
Travis Crampton <crampton.travis@gmail.com>
2018-12-29 18:32:50 -0800

    no no no sidekiq

--- app/controllers/journal_follows_controller.rb
+++ app/controllers/journal_follows_controller.rb
@@ -35,7 +35,7 @@ class JournalFollowsController < ApplicationController
     @chapter.update(email_sent: true)
 
     follower_emails.each do |email|
-      ChapterMailer.new_chapter(@chapter, email).deliver_later
+      ChapterMailer.new_chapter(@chapter, email).deliver_now
     end
 
     render json: chapter_json
