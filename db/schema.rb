0a9660db52313cdd2e97e52e8801e99cf27448cf
Travis Crampton <crampton.travis@gmail.com>
2019-03-15 11:23:32 -0500

    comments are loaded officially

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_11_134907) do
+ActiveRecord::Schema.define(version: 2019_03_15_160815) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,15 @@ ActiveRecord::Schema.define(version: 2019_03_11_134907) do
     t.datetime "date"
     t.boolean "published", default: false
     t.boolean "email_sent", default: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "commentable_id"
+    t.string "commentable_type"
+    t.integer "user_id"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "distances", force: :cascade do |t|
